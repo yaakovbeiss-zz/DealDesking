e555c6a1f0b034a2677b31ade1081ede0af9e46e
Yaakov Beiss <yaakovbeiss@gmail.com>
2017-08-28 13:41:02 -0400

    Add Search Bar

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -2,7 +2,7 @@ class Api::SearchController < ApplicationController
   def index
 
     if params[:query].present?
-      @leads = current_user.leads.where('lower(name) LIKE (?)', "%#{params[:query].downcase}%")
+      @leads = current_user.leads.where('lower(name) LIKE (?) OR lower(email) LIKE (?)', "%#{params[:query].downcase}%", "%#{params[:query].downcase}%")
     else
       @leads = current_user.leads
     end
--- frontend/components/searchbar/search_bar.jsx
+++ frontend/components/searchbar/search_bar.jsx
@@ -3,12 +3,30 @@ import React from 'react';
 class SearchBar extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      query: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    return e => {
+      this.setState({ query: e.currentTarget.value });
+      this.props.fetchSearch(e.currentTarget.value);
+    }
   }
 
   render() {
     return (
       <div>
         search bar
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+            value={this.state.query}
+            onChange={this.handleChange()}
+            />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }

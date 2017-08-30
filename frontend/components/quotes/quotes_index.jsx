48eb099f10b0a1c2f504df923821e951e2d4687a
Yaakov Beiss <yaakovbeiss@gmail.com>
2017-08-30 16:29:24 -0400

    Fix import

--- frontend/components/quotes/quotes_index.jsx
+++ frontend/components/quotes/quotes_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Quote from './quote';
-import Calculator '../calculator/calculator';
+import Calculator from '../calculator/calculator';
 
 class QuotesIndex extends React.Component {
   constructor(props){

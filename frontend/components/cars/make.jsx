44a67ab9a98c911a5386339010ef6da2c742826d
Yaakov Beiss <yaakovbeiss@gmail.com>
2017-08-31 15:53:16 -0400

    Toggle Car Components

--- frontend/components/cars/make.jsx
+++ frontend/components/cars/make.jsx
@@ -4,13 +4,26 @@ import ModelsIndex from '../cars/models_index';
 class Make extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      hideModels: true
+    }
+    this.toggleModels = this.toggleModels.bind(this);
+  }
+
+  toggleModels() {
+    this.setState({ hideModels: !this.state.hideModels })
+  }
+
+  models() {
+    return this.state.hideModels ? <div></div> :
+    <ModelsIndex models={this.props.models} />
   }
 
   render() {
     return (
       <div>
-        {this.props.name}
-        <ModelsIndex models={this.props.models} />
+        <button onClick={this.toggleModels}>{this.props.name}</button>
+        {this.models()}
       </div>
     )
   };
--- frontend/components/cars/model.jsx
+++ frontend/components/cars/model.jsx
@@ -4,16 +4,29 @@ import TrimsIndex from '../cars/trims_index';
 class Model extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      hideTrims: true
+    }
+    this.toggleTrims = this.toggleTrims.bind(this);
+  }
 
+  toggleTrims() {
+    this.setState({ hideTrims: !this.state.hideTrims })
+  }
+
+  trims() {
+    const trims = this.props.trims;
+    return this.state.hideTrims ? <div></div> :
+    <TrimsIndex trims={trims} />
   }
 
   render() {
     const name = this.props.name;
-    const trims = this.props.trims;
+
     return (
       <div>
-        {name}
-        <TrimsIndex trims={trims} />
+        <button onClick={this.toggleTrims}>{name}</button>
+        {this.trims()}
       </div>
     )
   }
